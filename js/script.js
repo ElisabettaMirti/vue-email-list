a595ee3acc86b0ff22227644859c4eee9127974d
ElisabettaMirti <belmirti@gmail.com>
2024-06-05 15:49:34 +0200

    fatto prova della funzione con API boolean per vedere se funziona

--- js/script.js
+++ js/script.js
@@ -3,8 +3,19 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
+            randomMailList: []
         }
     },
     methods: {
+        getRandomMails() {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((mail) => {
+                console.log(mail.data.response)
+            })
+        }
+    },
+
+    created(){
+        this.getRandomMails()
     }
 }).mount('#app');
